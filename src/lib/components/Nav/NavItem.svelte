8e6fe86c5676403ae589fa08b0d6b8c5da168d67
Jack Kufa <jack@kufa.io>
2024-01-09 18:22:13 -0500

    Fix some bugs

--- src/lib/components/Nav/NavItem.svelte
+++ src/lib/components/Nav/NavItem.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
-	import { cubicIn } from 'svelte/easing';
-	import { tweened } from 'svelte/motion';
 	import { SlideInOut } from '../Animations';
 	import { lenisInstance } from '..';
+  import { page } from '$app/stores';
 
 	/**
 	 * Must be an id, must be unique
 	 */
 	export let href: string;
 
-	function scrollTo() {
-		const target = href ? `#${href}` : 'top';
+	function scrollTo(e: Event) {
+    if ($page.url.pathname !== '/') return;
+
+		e.preventDefault();
+    const target = href ? `#${href}` : 'top';
 		$lenisInstance.scrollTo(target);
 	}
 </script>
@@ -29,7 +31,7 @@
     md:my-1
     md:p-4
     "
-		on:click|preventDefault={scrollTo}
+		on:click={scrollTo}
 	>
 		<slot />
 	</a>
