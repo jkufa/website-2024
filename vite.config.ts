cb48836485e06f6cc7c930884a1e1fd497aed118
Jack Kufa <jack@kufa.io>
2024-01-09 19:44:47 -0500

    add minify threeJS

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, type PluginOption } from 'vite';
-import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
+import { threeMinifier } from '@yushijinhun/three-minifier-rollup';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-    threeMinifier(),
+		threeMinifier(),
 		visualizer({
 			emitFile: true,
 			brotliSize: true,
