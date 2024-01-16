787d11d960be58b2760cad905a68984e41b93672
Jack Kufa <jack@kufa.io>
2024-01-15 19:37:38 -0500

    Remove enhancedImages cuz idk how to use it

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, type PluginOption } from 'vite';
 import { threeMinifier } from '@yushijinhun/three-minifier-rollup';
 import { visualizer } from 'rollup-plugin-visualizer';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		enhancedImages(),
 		threeMinifier(),
 		visualizer({
 			emitFile: true,
